45f44bf9aca77369260bd19392964c5561e8ec9c
hammerheart92 <hammerheart92@gmail.com>
2025-09-12 14:47:27 +0300

    android: enable core library desugaring for flutter_local_notifications

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -38,3 +39,9 @@ android {
 flutter {
     source = "../.."
 }
+
+dependecies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+}
+
+
